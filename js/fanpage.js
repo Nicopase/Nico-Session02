0c9c7e54015a801630dd856228090ffd95e19947
Nico Paredes <pared055@umn.edu>
2023-01-23 13:01:37 -0600

    fixed problems

--- js/fanpage.js
+++ js/fanpage.js
@@ -1,8 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    getCatImage()
-    btnClick()
-})
-
 function getCatImage(){
     let image= document.getElementById("cat-image");
     fetch('https://api.thecatapi.com/v1/images/search?api_key=live_PsFV5Yqlsoxq5r8i4vdPrUrOCCsRJZDkQkaMzcmQ6pIIGXo7c47UaatVlIvCVgo1')
